2e45dfd1247b312e9f91be98c023ce25d584def2
yjlim <yjlim@hdc-labs.com>
2025-02-20 15:04:31 +0900

    chore: 프로젝트 초기 세팅

---
+++ backend/src/main/java/com/project/OliveYoungApplication.java
@@ -0,0 +1,13 @@
+package com.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OliveYoungApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OliveYoungApplication.class, args);
+	}
+
+}
